676ac5007a05bd66e0b8deac39d04126e4fc8e7b
marko <markotaht@gmail.com>
2018-11-03 19:34:30 +0200

    Animations

--- ProjectX/Assets/Scripts/CharacterControllerRb.cs
+++ ProjectX/Assets/Scripts/CharacterControllerRb.cs
@@ -14,37 +14,81 @@ public class CharacterControllerRb : MonoBehaviour {
 
     public JumpModifiers jumpModifiers;
 
+    private Animator animator;
+
     bool grounded = false;
     public Transform groundCheck;
-    float groundRadius = 0.2f;
+    float groundRadius = 0.1f;
     public LayerMask whatIsGround;
 
+    bool facingRight = false;
+    bool inAir = false;
 
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
         jumpModifiers = new JumpModifiers();
+        animator = GetComponentInChildren<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         if (grounded && Input.GetKeyDown(KeyCode.Space))
         {
-            rb.AddForce(new Vector2(0, jumpModifiers.ApplyModifiers(jumpforce)));
+            animator.SetBool("Jump up", true);
         }
+    }
+
+    public void Jump()
+    {
+        animator.SetBool("Jump up", false);
+        inAir = true;
+        rb.AddForce(new Vector2(0, jumpModifiers.ApplyModifiers(jumpforce)));
+    }
+
+    public void Land()
+    {
+        inAir = false;
+        animator.SetBool("Falling", true);
+    }
+
+    public void Landed()
+    {
+        animator.SetBool("Falling", false);
     }
 
     private void FixedUpdate()
     {
         grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
-
+       
         float move = rb.velocity.x / maxSpeed;
-        if (grounded)
+        if(grounded && inAir && rb.velocity.y < 0)
         {
-            move = Input.GetAxis("Horizontal");
+            Land();
+        }
+        else if (grounded)
+        {
+            move = Input.GetAxisRaw("Horizontal");
+            animator.SetFloat("Walking", Mathf.Abs(move));
+
+            if(move > 0 && !facingRight)
+            {
+                Flip();
+            }else if(move < 0 && facingRight)
+            {
+                Flip();
+            }
         }
 
         rb.velocity = new Vector3(move * maxSpeed, rb.velocity.y);
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -54,7 +98,11 @@ public class CharacterControllerRb : MonoBehaviour {
 
         RaycastHit2D hitCeiling = Physics2D.BoxCast(transform.position, GetComponent<BoxCollider2D>().size, 0, Vector2.up, 1.2f);
 
-        if(!grounded && collisionDir.y > 0 && hitCeiling.collider != null)
+        if (!grounded && !Mathf.Approximately(collisionDir.x, 0) && !Mathf.Approximately(rb.velocity.x, 0))
+        {
+            Die(false);
+        }
+        else if (!grounded && collisionDir.y > 0 && hitCeiling.collider != null)
         {
             Debug.Log(hitCeiling.collider.name);
             Die(true);
@@ -69,12 +117,17 @@ public class CharacterControllerRb : MonoBehaviour {
     {
         if (ceiling)
         {
+            animator.SetBool("HitCeiling", true);
+            rb.gravityScale = 0;
+            rb.velocity = Vector3.zero;
             //Ceiling death animation
             Debug.Log("ceiling death");
         }
         else
         {
-            //Wall death animation
+            animator.SetBool("HitWall", true);
+            rb.gravityScale = 0;
+            rb.velocity = Vector3.zero;
             Debug.Log("wall death");
         }
     }
