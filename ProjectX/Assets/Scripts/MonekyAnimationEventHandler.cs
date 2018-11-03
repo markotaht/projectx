dd30e2af44e18dd5771860b6120da248de5310bd
DAlgma <dianx93@gmail.com>
2018-11-04 00:54:29 +0200

    soundcontroller

--- ProjectX/Assets/Scripts/MonekyAnimationEventHandler.cs
+++ ProjectX/Assets/Scripts/MonekyAnimationEventHandler.cs
@@ -6,14 +6,31 @@ public class MonekyAnimationEventHandler : MonoBehaviour {
 
     [SerializeField]
     CharacterControllerRb cc;
+    PlayerSoundController psc;
 
 	void Jump()
     {
         cc.Jump();
+        psc.Jump();
     }
 
     void Landed()
     {
         cc.Landed();
+    }
+    
+    void Die()
+    {
+        psc.Die();
+    }
+
+    void Run(bool start)
+    {
+        psc.Running(start);
+    }
+
+    void Fart()
+    {
+        psc.Fart();
     }
 }
---
+++ ProjectX/Assets/Scripts/PlayerSoundController.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSoundController : MonoBehaviour {
+    public AudioSource eating;
+    public AudioSource jumping;
+    public AudioSource running;
+    public AudioSource dying;
+    public AudioSource farting;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Eat()
+    {
+        eating.Play();
+    }
+
+    public void Jump()
+    {
+        jumping.Play();
+    }
+
+    public void Die()
+    {
+        dying.Play();
+    }
+
+    public void Running(bool start)
+    {
+        if (start)
+        {
+            running.Play();
+        }
+        else
+        {
+            running.Stop();
+        }
+    }
+
+    public void Fart()
+    {
+        farting.Play();
+    }
+
+}
